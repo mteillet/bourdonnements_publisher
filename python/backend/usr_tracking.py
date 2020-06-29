d03db8e960a5370a4f7e6374ddd16f1e7a7d8293
mteillet <martin.teillet@hotmail.fr>
2020-06-29 15:21:27 +0200

    started bourdonnements publisher

---
+++ python/backend/usr_tracking.py
@@ -0,0 +1,7 @@
+import os
+
+def main():
+    print("user tracking")
+
+if __name__ == "__main__":
+    main()
---
+++ python/bourdonnement_publisher_v01.py
@@ -0,0 +1,10 @@
+import os
+import maya.cmds as cmds
+import usr_tracking as ut
+
+def main():
+    print("hello world!")
+
+
+if __name__ == "__main__":
+    main()
