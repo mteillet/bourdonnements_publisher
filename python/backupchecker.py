fb3db0ad223ee3cd0d4ccd638f0f543c83ecbe0f
mteillet <martin.teillet@hotmail.fr>
2020-06-29 19:57:13 +0200

    Added the backupchecker.py

---
+++ python/backupchecker.py
@@ -0,0 +1,8 @@
+import os
+#import maya.cmds as cmds
+
+def main():
+    print("BackupChecker")
+
+if __name__ == "__main__":
+    main()
--- python/bourdonnement_publisher_v01.py
+++ python/bourdonnement_publisher_v01.py
@@ -1,11 +1,13 @@
 import os
 #import maya.cmds as cmds
 import usr_tracking as ut
+import backupchecker as bckpcheck
 
 def main():
     # Read the usrList.json using the usr_tracking.py file
     userList = ut.main()
     print(userList)
+    bckpcheck.main()
 
 
 if __name__ == "__main__":
