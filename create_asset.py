c9f58b05c1742249f8ee43547a9f92fe87aa2ed2
mteillet <martin.teillet@hotmail.fr>
2020-07-07 00:15:58 +0200

    added create_asset.py

---
+++ create_asset.py
@@ -0,0 +1,65 @@
+#### Function responsible for the creation of an asset
+
+#### Define if asset is character of prop
+
+# If asset is a character
+#   ask for character name
+# create the following folder structure
+
+#Character/Prop
+#    blender
+#        input
+#        output
+#    houdini
+#        abc
+#        audio
+#        comp
+#        desk
+#        flip
+#        geo
+#        hdz
+#        render
+#        scripts
+#        sim
+#        tex
+#        video
+#    maya
+#        cache
+#        data
+#        images
+#        movies
+#        scenes
+#        sound
+#        sourceimages
+#        workspace.mel
+#    nuke
+#        input
+#        output
+#    paint_2D
+#        input
+#        output
+#    paint_3D
+#        input
+#            obj
+#            sourceimages
+#        output
+#    photogrammetry
+#        input
+#        output
+#    preprod
+#    review
+#    sculpt
+#        input
+#        output
+#    wrap
+#        input
+#        output
+
+def main():
+    print("Create Asset")
+
+def getName():
+    print("getName")
+
+if __name__ == "__main__":
+    main()
